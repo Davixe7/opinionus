ed3d4c308d101ab375bc72637f022e514b1d3291
David <davidguilarte7@gmail.com>
2020-05-06 21:24:05 -0430

    Stable

--- resources/views/surveys/results.blade.php
+++ resources/views/surveys/results.blade.php
@@ -2,6 +2,12 @@
 @section('head')
 <meta property="og:image" content="{{ url(str_replace('public', '/storage', $survey->choices->first()->image)) }}" />
 <meta property="og:title" content="{{ $survey->name }}">
+<meta property="og:description" content="Come take this survey now" />
+
+<meta name="twitter:card" content="summary_large_image" />
+<meta name="twitter:title" content="{{ $survey->name }}" />
+<meta name="twitter:description" content="Come take this survey now" />
+<meta name="twitter:image:src" content="{{ url(str_replace('public', '/storage', $survey->choices->first()->image)) }}" />
 @endsection
 
 @section('content')
