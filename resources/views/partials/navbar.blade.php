dc372dc6db98d550b8a7ab9f415f19e7f028d155
David <davidguilarte7@gmail.com>
2020-07-05 18:19:52 -0430

    Enhanced navbar state management

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,16 +2,23 @@
   <button class="navbar-nav-toggler btn btn-navbar" data-target="#navbar-nav-tile">
     <i class="material-icons">menu</i>
   </button>
-  @if( isset($page_title) )
+  @if( isset($page_title) )  
     <span class="navbar-title">{{ $page_title }}</span>
   @else
     <a href="/" class="surveys-navbar-brand">
       <img src="/logo.png" alt="">
     </a>
   @endif
-  <button class="btn btn-navbar ml-auto">
-    <i class="material-icons">keyboard_backspace</i>
-  </button>
+  @if( Route::currentRouteName() == 'landing')
+  <a href="{{ route('search') }}" class="btn btn-navbar ml-auto">
+    <i class="material-icons">search</i>
+  </a>
+  @else
+    <a href="{{ url()->previous() }}" class="btn btn-navbar ml-auto">
+      <i class="material-icons">keyboard_backspace</i>
+    </a>
+  @endif
+  
   <div id="navbar-nav-tile" class="navbar-nav-tile">
     <div class="header">
       <button class="btn btn-outlined-default" onclick="document.querySelector('.navbar-nav-tile').classList.remove('active');">
