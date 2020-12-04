389ee1b6a2714bda868c73a5421ab439855de902
David Guilarte <davidguilarte7@gmail.com>
2020-12-04 08:30:05 -0500

    Added favicon. Removed old favicon resources.

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -3,6 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" type="image/png" href="/favicon.png">
   <meta name="csrf-token" content="{{ csrf_token() }}">
   <title>{{ config('APP_NAME', 'OpinionUS') }}</title>
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
